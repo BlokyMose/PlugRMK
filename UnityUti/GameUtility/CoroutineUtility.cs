c47afe749fec865206dd32df8efb83f5b66ca306
Ray <blokymose58@gmail.com>
2024-08-25 04:24:08 +0900

    CoroutineUtility: Delay, Update

--- UnityUti/GameUtility/CoroutineUtility.cs
+++ UnityUti/GameUtility/CoroutineUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace PlugRMK.UnityUti
@@ -14,6 +15,33 @@ namespace PlugRMK.UnityUti
         public static void StopCoroutineIfExists(this MonoBehaviour go, Coroutine routine)
         {
             if (routine != null) go.StopCoroutine(routine);
+        }
+
+        public static IEnumerator Delay(float delay, Action onFinished)
+        {
+            yield return new WaitForSeconds(delay);
+            onFinished();
+        }
+
+        public static void StartDelay(this MonoBehaviour go, float delay, Action onFinished)
+        {
+            go.StartCoroutine(Delay(delay, onFinished));
+        }
+
+        public static IEnumerator Update(Func<float, float> onUpdate)
+        {
+            var time = 0f;
+            while (true)
+            {
+                time = onUpdate(time);
+                time += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        public static void StartUpdate(this MonoBehaviour go, Func<float, float> onUpdate)
+        {
+            go.StartCoroutine(Update(onUpdate));
         }
     }
 }
